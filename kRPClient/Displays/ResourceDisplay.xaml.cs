01956bcc4ada95226b4b569cba6b4f7e8f8d2c5e
Nils Hambrecht <pchcku@nhdn.de>
2016-02-04 21:24:51 +0100

    Generalized resource display

--- kRPClient/Displays/ResourceDisplay.xaml.cs
+++ kRPClient/Displays/ResourceDisplay.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 
@@ -9,112 +10,42 @@ namespace kRPClient.Displays
     /// </summary>
     public partial class ResourceDisplay : UserControl
     {
-        // Using a DependencyProperty as the backing store for MyHeader.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty MyHeaderProperty =
-            DependencyProperty.Register("MyHeader", typeof(string), typeof(ResourcesDisplay), new PropertyMetadata(default(string), new PropertyChangedCallback(OnLabelChanged)));
-
-        // Using a DependencyProperty as the backing store for ResourcesShown.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ResourcesShownProperty =
-            DependencyProperty.Register("ResourcesShown", typeof(string), typeof(ResourcesDisplay), new PropertyMetadata(default(string), new PropertyChangedCallback(OnResourcesShownChanged)));
-
-        private bool hasInit = false;
+        public static readonly DependencyProperty ResourceItemsProperty =
+            DependencyProperty.Register("ResourceItems", typeof(string), typeof(ResourceDisplay), new PropertyMetadata(OnItemsSourceChanged));
 
         public ResourceDisplay()
         {
             InitializeComponent();
         }
 
-        public string MyHeader
+        public string ResourceItems
         {
-            get { return (string)GetValue(MyHeaderProperty); }
-            set { SetValue(MyHeaderProperty, value); HeaderLabel.Content = value; }
-        }
-
-        public string ResourcesShown
-        {
-            get { return (string)GetValue(ResourcesShownProperty); }
+            get
+            {
+                return (string)GetValue(ResourceItemsProperty);
+            }
             set
             {
-                SetValue(ResourcesShownProperty, value);
-                //BuildResourceDisplay();
+                SetValue(ResourceItemsProperty, value);
             }
         }
 
-        private static void OnLabelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
         {
-            ResourcesDisplay resourcesDisplay = (ResourcesDisplay)d;
-            resourcesDisplay.Content = (string)e.NewValue;
-        }
+            base.OnPropertyChanged(e);
 
-        private static void OnResourcesShownChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            ResourcesDisplay resourcesDisplay = (ResourcesDisplay)d;
-            resourcesDisplay.ResourcesShown = (string)e.NewValue;
-        }
-
-        private void AddToGridAt(UIElement element, int row, int column)
-        {
-            Grid.SetColumn(element, column);
-            Grid.SetRow(element, row);
-            ResourceGrid.Children.Add(element);
-        }
-
-        private void BuildResourceDisplay()
-        {
-            if (!hasInit && !string.IsNullOrWhiteSpace(ResourcesShown))
+            if (e.Property == DataContextProperty)
             {
-                var resourcesToDisplay = ResourcesShown.Split(new char[] { ',' });
-
-                GridLength rowHeight = new GridLength(32);
-                int currentRow = 1;
-                //ResourceGrid.Height = 32 * (resourcesToDisplay.Length + 1);
-                //ResourceGrid.ShowGridLines = true;
-                var overviewModel = ((MainWindow)((Grid)this.Parent).Parent).DataContext;
-
-                foreach (var resource in resourcesToDisplay)
-                {
-                    RowDefinition row = new RowDefinition() { Height = rowHeight };
-                    ResourceGrid.RowDefinitions.Add(row);
-
-                    Label name = new Label() { Content = resource };
-                    Binding nameBinding = new Binding(string.Format("Resources.Resources[\"{0}\"].Name", resource));
-                    nameBinding.Source = this;
-                    name.SetBinding(Label.ContentProperty, nameBinding);
-                    AddToGridAt(name, currentRow, 0);
-
-                    Binding maximumBinding = new Binding(string.Format("Resources.Resources[\"{0}\"].Maximum", resource));
-                    maximumBinding.Source = this;
-                    Binding valueBinding = new Binding(string.Format("Resources.Resources[\"{0}\"].Ammount", resource));
-                    valueBinding.Source = this;
-
-                    ProgressBar bar = new ProgressBar()
-                    {
-                        Minimum = 0
-                    };
-                    bar.SetBinding(ProgressBar.MaximumProperty, maximumBinding);
-                    bar.SetBinding(ProgressBar.ValueProperty, valueBinding);
-                    AddToGridAt(bar, currentRow, 1);
-
-                    Label ammountLabel = new Label();
-                    ammountLabel.SetBinding(Label.ContentProperty, valueBinding);
-                    ammountLabel.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
-                    AddToGridAt(ammountLabel, currentRow, 2);
-
-                    Label slashLabel = new Label() { Content = "/" };
-                    AddToGridAt(slashLabel, currentRow, 3);
-
-                    Label maximumLabel = new Label();
-                    maximumLabel.SetBinding(Label.ContentProperty, maximumBinding);
-                    AddToGridAt(maximumLabel, currentRow, 4);
-
-                    currentRow++;
-                }
-                ResourceGrid.InvalidateVisual();
-                ResourceGrid.UpdateLayout();
-                this.InvalidateVisual();
-                this.UpdateLayout();
-                hasInit = true;
+                Binding myBinding = new Binding(string.Format("Resources.{0}", ResourceItems));
+                myBinding.Source = DataContext;
+                DisplayGrid.SetBinding(DataGrid.ItemsSourceProperty, myBinding);
             }
+        }
+
+        private static void OnItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ResourceDisplay resourcesDisplay = (ResourceDisplay)d;
+            resourcesDisplay.ResourceItems = (string)e.NewValue;
         }
     }
 }
