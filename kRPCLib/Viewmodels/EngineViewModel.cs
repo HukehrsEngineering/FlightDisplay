872079bff796207bb2bad10b4d949a0b6391a563
HukehrsEngineering <hukeng@nhdn.de>
2017-03-23 20:54:10 +0100

    added engine view model

---
+++ kRPCLib/Viewmodels/EngineViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KRPC.Client.Services.SpaceCenter;
+
+namespace kRPCLib.Viewmodels
+{
+    public class EngineViewModel : BaseViewModel
+    {
+        /*private IList<Engine> _engines;
+        private IList<> _engines;
+
+        public void Init(Vessel vessel)
+        {
+            Engines = vessel.Parts.Engines;
+            Intakes = vessel.Parts.Intakes;
+            Tanks = vessel.Parts.All.Where(p => p.Resources.)
+        }
+
+        public IList<Intake> Intakes { get; set; }
+
+        public IList<Engine> Engines
+        {
+            get { return _engines; }
+            set { _engines = value; OnPropertyChanged(); }
+        }
+
+        public IList<Engine> Tanks
+        {
+            get { return _engines; }
+            set { _engines = value; OnPropertyChanged(); }
+        }*/
+    }
+}
